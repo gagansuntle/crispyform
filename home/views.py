9ae1db1867da16a11e904c3a6a08338b2009db21
gagansuntle <gagan@suntle.com>
2017-05-10 18:53:21 +0530

    custom contact form

---
+++ home/views.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+from django.conf import settings
+from django.core.mail import send_mail
+
+from django.shortcuts import render
+from .forms import ContactForm
+
+# Create your views here.
+
+def contact(request):
+	title = 'Contact Us'
+	title_align_center = True
+	form = ContactForm(request.POST or None)
+	if form.is_valid():
+		# for key, value in form.cleaned_data.iteritems():
+		# 	print key, value
+		# 	#print form.cleaned_data.get(key)
+		form_email = form.cleaned_data.get("email")
+		form_message = form.cleaned_data.get("message")
+		form_full_name = form.cleaned_data.get("full_name")
+		# print email, message, full_name
+		subject = 'Site contact form'
+		from_email = settings.EMAIL_HOST_USER
+		to_email = [from_email, 'gagan@suntle.com']
+		contact_message = "%s: %s via %s"%( 
+				form_full_name, 
+				form_message, 
+				form_email)
+		
+		send_mail(subject, 
+				contact_message, 
+				from_email, 
+				to_email, 
+				fail_silently=True)
+
+	context = {
+		"form": form,
+		"title": title,
+		"title_align_center": title_align_center,
+	}
+	return render(request, "forms.html", context)
+	
